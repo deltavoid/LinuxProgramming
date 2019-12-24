1c5483f76232fd2138aa7fe9b2735e848e93b98d
zqy <zqy@debian10-dev.localdomain>
2019-12-24 11:16:59 +0800

    update tasklet

--- module/tasklet/tasklet.c
+++ module/tasklet/tasklet.c
@@ -6,22 +6,34 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/version.h>
+#include <linux/interrupt.h>
 
 
 
-
-static __init int tasklet_init(void)
+void tasklet_hello(unsigned long arg)
 {
+    pr_info("%s: %lx\n", __func__, arg);
+}
+
+DECLARE_TASKLET(my_tasklet, tasklet_hello, 0x12345678);
+
+
+
+static __init int tasklet_module_init(void)
+{
+    pr_info("%s: 1\n", __func__);
+    tasklet_schedule(&my_tasklet);
+
     pr_info("tasklet inserted\n");
     return 0;
 }
 
-static __exit void tasklet_exit(void)
+static __exit void tasklet_module_exit(void)
 {
     pr_info("tasklet removed\n");
 }
 
 
-module_init(tasklet_init);
-module_exit(tasklet_exit);
+module_init(tasklet_module_init);
+module_exit(tasklet_module_exit);
 MODULE_LICENSE("GPL");
