79b0d41c160ac61e8a8727bca014e11e5148739d
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2020-01-03 15:23:34 +0800

    clear comment in irq_test

--- module/irq_test/irq_test.c
+++ module/irq_test/irq_test.c
@@ -13,36 +13,14 @@
 static int irq; /*模块参数-中断号*/
 module_param(irq, int, 0644);
 
-// static char *interface; /*模块参数-设备名*/
-// module_param(interface, char*, 0644);
 static char dev_name[] = "test";
-
 static long count = 0; /*统计插入模块期间发生的中断次数*/
 static long last_jiffies;
 
 
 
-
-
-
-
 static irqreturn_t intr_handler(int irq, void *dev_id)
 {
-    // static long interval = 0;
-
-    // if(count==0)
-    // {
-    //     interval=jiffies;
-    // }
-    
-    // /*计算两次中断之间的间隔,时间单位为节拍 */
-    // interval = jiffies - interval; 
-    
-    // // pr_info("The interval between two interrupts is %ld\n" interval);
-
-    // interval=jiffies;
-    
-
     count++;
 
     if  (count % 10 == 0)
@@ -58,16 +36,8 @@ static irqreturn_t intr_handler(int irq, void *dev_id)
 
 
 
-
-
 static int __init irq_test_init(void)
 {
-    // /*注册中断服务程序，注意内核版本不同，共享标志可能有所不同*/
-    // if (request_irq(irq, &intr_handler, IRQF_SHARED, interface, &irq)) {
-    //     pr_err("Fails to register IRQ %dn", irq);
-    //     return -EIO;
-    // }
-    // pr_info("%s Request on IRQ %d succeededn",interface,irq);
     pr_info("irq: %d, dev: %lx\n", irq, &irq);
 
     if  (request_irq(irq, &intr_handler, IRQF_SHARED, dev_name, &irq) != 0)
@@ -80,10 +50,6 @@ static int __init irq_test_init(void)
 
 static void __exit irq_test_exit(void)
 {
-    // pr_info("The %d interrupts happened on irq %d", count, irq);
-    // free_irq(irq, &irq); /* 释放中断线*/
-    // pr_info("Freeing IRQ %d\n", irq);
-
     if  (free_irq(irq, &irq) == NULL)
     {   pr_warn("free_irq %d failed\n", irq); 
     }
