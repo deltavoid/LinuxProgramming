fba75c0c5e47d0330c891fc8bc9ecfc6eb8a0cdf
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2020-02-05 20:22:24 +0800

    client_epoll report tx rx throughout

--- network/tcp_echo/client_epoll.cpp
+++ network/tcp_echo/client_epoll.cpp
@@ -5,6 +5,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <ctime>
+
 #include <vector>
 #include <memory>
 #include <thread>
@@ -13,6 +15,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/epoll.h>
+#include <sys/time.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <pthread.h>
@@ -28,21 +31,76 @@ struct sockaddr_in dst_addr;
 int thread_num = 1;
 int conn_num = 1;
 int pkt_size = 64;
-int req_num = 1000;
+int duration = 10;
 
 
+
+class Tracer
+{public:
+    enum Item
+    {   TX_PKT = 0,
+        RX_PKT,
+        TX_BYTE,
+        RX_BYTE,
+        INDEX_MAX,
+    };
+    static const char* item_name[];
+    
+    unsigned long long data[INDEX_MAX];
+    struct timeval stamp;
+
+    Tracer()
+    {
+        memset(data, 0, sizeof(data));
+        get_time();
+    }
+
+
+    ~Tracer() {}
+
+    void get_time()  {  gettimeofday(&stamp, NULL);}
+
+    void inc(int idx)  {  data[idx]++;}
+    void add(int idx, int val)  {  data[idx] += val;}
+
+
+    static void report(Tracer* now, Tracer* old)
+    {
+        double dt_time = now->stamp.tv_usec - old->stamp.tv_usec 
+                + (now->stamp.tv_sec - old->stamp.tv_sec) * 1000 * 1000;
+
+        // printf("dt_time: %lf\n", dt_time);
+        
+        for (int i = 0; i < INDEX_MAX; i++)
+        {
+            double dt_val = now->data[i] - old->data[i];
+            double speed = dt_val * 1000 * 1000 / dt_time;
+            printf("%s: %.2lf\n", item_name[i], speed);
+        }
+        printf("\n");
+
+        *old = *now;
+    }
+};
+
+const char* Tracer::item_name[] = {
+    "tx pkt/s",
+    "rx pkt/s",
+    "tx byte/s",
+    "rx byte/s",
+};
 
 
 class Connection
 {public:
     int fd;
-    int req_num;
     int pkt_size;
     char* tx_buf;
     char* rx_buf;
+    Tracer* tracer;
 
-    Connection(int fd, int pkt_size, int req_num, char* tx_buf, char* rx_buf)
-        : fd(fd), pkt_size(pkt_size), req_num(req_num), tx_buf(tx_buf), rx_buf(rx_buf)
+    Connection(int fd, int pkt_size, char* tx_buf, char* rx_buf, Tracer* tracer)
+        : fd(fd), pkt_size(pkt_size), tx_buf(tx_buf), rx_buf(rx_buf), tracer(tracer)
     {
     }
 
@@ -54,11 +112,15 @@ class Connection
     void send()
     {
         int sent = ::send_full(fd, tx_buf, pkt_size, 0);
+        tracer->inc(tracer->TX_PKT);
+        tracer->add(tracer->TX_BYTE, sent);
     }
 
     void recv()
     {
         int recd = ::recv(fd, rx_buf, pkt_size, 0);
+        tracer->inc(tracer->RX_PKT);
+        tracer->add(tracer->RX_BYTE, recd);
         // printf("fd %d recv %d bytes\n", fd, recd);
     }
 
@@ -83,7 +145,9 @@ class EventLoop
     bool running;
     std::unique_ptr<std::thread> thread;
 
-    EventLoop(struct sockaddr* addr, int conn_num, int pkt_size, int req_num)
+    Tracer tracer;
+
+    EventLoop(struct sockaddr* addr, int conn_num, int pkt_size)
         : events(conn_num), tx_buf(max_pkt_size), rx_buf(max_pkt_size)
     {
         if  ((epfd = epoll_create1(0)) < 0)  perror("epoll_create1 error");
@@ -102,10 +166,9 @@ class EventLoop
             if  (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &event) < 0)  perror("epoll add error");
             
 
-            conns.push_back(std::make_unique<Connection>(fd, pkt_size, req_num, tx_buf.data(), rx_buf.data()));
+            conns.push_back(std::make_unique<Connection>(fd, pkt_size, tx_buf.data(), rx_buf.data(), &tracer));
 
             printf("establish connection on fd %d\n", fd);
-
         }
 
         running = true;
@@ -118,6 +181,7 @@ class EventLoop
     ~EventLoop()
     {
         thread->join();
+        close(epfd);
     }
 
 
@@ -142,7 +206,7 @@ int parse_arg(int argc, char** argv)
 {
     if  (argc < 3)
     {
-        printf("usage: %s <dst ip> <dst port> [<threads> <conn per thread> <packet size> <req per conn>]\n", argv[0]);
+        printf("usage: %s <dst ip> <dst port> [<threads> <conn per thread> <packet size> <duration>]\n", argv[0]);
         return 1;
     }
 
@@ -164,9 +228,9 @@ int parse_arg(int argc, char** argv)
         if  (sscanf(argv[5], "%d", &pkt_size) < 0)  perror("bad pkt_size");
 
     if  (argc > 6)
-        if  (sscanf(argv[6], "%d", &req_num) < 0)  perror("bad req num");
+        if  (sscanf(argv[6], "%d", &duration) < 0)  perror("bad duration");
 
-    printf("%d %d %d %d\n", thread_num, conn_num, pkt_size, req_num);
+    printf("%d %d %d %d\n", thread_num, conn_num, pkt_size, duration);
     return 0;
 }
 
@@ -174,9 +238,21 @@ int main(int argc, char** argv)
 {
     if  (parse_arg(argc, argv) < 0)  return 0;
     
-    EventLoop loop((struct sockaddr*)&dst_addr, conn_num, pkt_size, req_num);
+    EventLoop loop((struct sockaddr*)&dst_addr, conn_num, pkt_size);
 
+    Tracer old;
+    for (int i = 0; i < duration; i++)
+    {
+        sleep(1);
+        
+        Tracer now(loop.tracer);
+        now.get_time();
+        
+        Tracer::report(&now, &old);
+    }
 
+    loop.running = false;
+    // loop dtor
 
     return 0;
 }
