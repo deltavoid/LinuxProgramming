8afe621db6693a384f944d18c2379f82679dc08c
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2020-03-03 00:53:08 +0800

    add proc_entry1 with bar, which supports read and write

--- module/proc_entry1/proc_entry1.c
+++ module/proc_entry1/proc_entry1.c
@@ -18,12 +18,12 @@
 
 
 
-// #define FOOBAR_LEN 64
+#define FOOBAR_LEN 60
 
-// struct fb_data{
-// 	char name[FOOBAR_LEN];
-// 	char value[FOOBAR_LEN];
-// };
+struct fb_data{
+	char name[FOOBAR_LEN];
+	char value[FOOBAR_LEN];
+};
 
 
 // foo ---------------------------------------------------------
@@ -62,14 +62,46 @@
 // bar --------------------------------------------------------------
 
 
-// static const struct file_operations foo_entry_fops = 
-// {
-//     .owner      = THIS_MODULE,
-//     .open       = pet_seq_open,
-//     .read       = seq_read,
-//     .llseek     = seq_lseek,
-//     .release    = single_release,
-// };
+static struct fb_data bar_data = { "bar", "blank"};
+
+static ssize_t bar_entry_read(struct file *fp, char __user *buf, size_t size, loff_t *offp )
+{
+    char res[128];
+    int len;
+
+    if  (*offp > 0)  return 0;
+
+    len = sprintf(res, "%s is %s", bar_data.name, bar_data.value);
+    if  (len < 0)  return -1;
+
+    if  (size < len)  return -1;
+
+    if  (copy_to_user(buf, res, len) != 0)  return -1;
+    
+    // operation per file
+    *offp += len;
+
+    return len;
+}
+
+static ssize_t bar_entry_write(struct file *fp, const char __user *buf, size_t size, loff_t *offp)
+{
+    if  (size > FOOBAR_LEN - 1)  return -1;
+
+    if  (copy_from_user(bar_data.value, buf, size) != 0)  return -1;
+    bar_data.value[size] = '\0';
+
+    *offp += size;
+
+    return size;
+}
+
+static const struct file_operations bar_entry_fops = 
+{
+    .owner      = THIS_MODULE,
+    .read       = bar_entry_read,
+    .write      = bar_entry_write,
+};
 
 // jiffies ----------------------------------------------------------
 
@@ -91,6 +123,7 @@ static ssize_t jiffies_entry_read(struct file *fp, char __user *buf, size_t size
     
     // operation per file
     *offp += len;
+
     return len;
 }
 
@@ -128,10 +161,15 @@ static int __init proc_entry1_init(void)
     jiffies_entry = proc_create("jiffies", 0444, example_entry, &jiffies_entry_fops);
     if  (!jiffies_entry)  goto err_jiffies;
 
+    bar_entry = proc_create("bar", 0, example_entry, &bar_entry_fops);
+    if  (!bar_entry)  goto err_bar;
 
     pr_info("proc_entry1 inserted\n");
     return 0;
 
+    
+err_bar:
+    remove_proc_entry("jiffies", example_entry);
 err_jiffies:
     remove_proc_entry("example", NULL);
 err_example:
@@ -140,6 +178,7 @@ err_example:
 
 static void __exit proc_entry1_exit(void)
 {
+    remove_proc_entry("bar", example_entry);
     remove_proc_entry("jiffies", example_entry);
 	remove_proc_entry("example", NULL);
     
