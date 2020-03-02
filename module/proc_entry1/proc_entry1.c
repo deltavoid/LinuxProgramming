dca63784eafc7a05340d0f8cffa7872897db2978
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2020-03-03 01:05:15 +0800

    add proc_entry1 with foo, which supports seq_put and seq_printf

--- module/proc_entry1/proc_entry1.c
+++ module/proc_entry1/proc_entry1.c
@@ -28,36 +28,49 @@ struct fb_data{
 
 // foo ---------------------------------------------------------
 
-// static int pet_entry_show(struct seq_file *seq, void *arg)
-// {
-//     int i;
+struct fb_data foo_data = {"foo", "blank"};
 
-//     pr_debug("pet_entry_show\n");
+static int foo_entry_show(struct seq_file *seq, void *arg)
+{
+    pr_debug("foo_entry_show\n");
+
+    seq_puts(seq, "hello, ");
     
-//     seq_puts(seq, "proc entry0 hello\n");
-
-//     for (i = 0; i < 10; i++)
-//         seq_puts(seq, "proc entry0 world\n");
-
-//     return 0;
-// }
-
-// static int pet_seq_open(struct inode *inode, struct file *file)
-// {
-//     pr_debug("pet_seq_open\n");
-
-//     return single_open(file, pet_entry_show, NULL);
-// }
+    seq_printf(seq, "%s is %s", foo_data.name, foo_data.value);
 
 
-// static const struct file_operations foo_entry_fops = 
-// {
-//     .owner      = THIS_MODULE,
-//     .open       = pet_seq_open,
-//     .read       = seq_read,
-//     .llseek     = seq_lseek,
-//     .release    = single_release,
-// };
+    return 0;
+}
+
+static int foo_entry_open(struct inode *inode, struct file *file)
+{
+    pr_debug("foo_entry_open\n");
+
+    return single_open(file, foo_entry_show, NULL);
+}
+
+static ssize_t foo_entry_write(struct file *fp, const char __user *buf, size_t size, loff_t *offp)
+{
+    if  (size > FOOBAR_LEN - 1)  return -1;
+
+    if  (copy_from_user(foo_data.value, buf, size) != 0)  return -1;
+    foo_data.value[size] = '\0';
+
+    *offp += size;
+
+    return size;
+}
+
+
+static const struct file_operations foo_entry_fops = 
+{
+    .owner      = THIS_MODULE,
+    .open       = foo_entry_open,
+    .read       = seq_read,
+    .llseek     = seq_lseek,
+    .release    = single_release,
+    .write      = foo_entry_write,
+};
 
 // bar --------------------------------------------------------------
 
@@ -164,10 +177,14 @@ static int __init proc_entry1_init(void)
     bar_entry = proc_create("bar", 0, example_entry, &bar_entry_fops);
     if  (!bar_entry)  goto err_bar;
 
+    foo_entry = proc_create("foo", 0, example_entry, &foo_entry_fops);
+    if  (!foo_entry)  goto err_foo;
+
     pr_info("proc_entry1 inserted\n");
     return 0;
 
-    
+err_foo:
+    remove_proc_entry("bar", example_entry);
 err_bar:
     remove_proc_entry("jiffies", example_entry);
 err_jiffies:
@@ -178,6 +195,7 @@ err_example:
 
 static void __exit proc_entry1_exit(void)
 {
+    remove_proc_entry("foo", example_entry);
     remove_proc_entry("bar", example_entry);
     remove_proc_entry("jiffies", example_entry);
 	remove_proc_entry("example", NULL);
