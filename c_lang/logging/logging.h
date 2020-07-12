6354de1001652319309989bcae15d543a02105e5
root <root@n3-048-205.byted.org>
2020-07-12 23:38:18 +0800

    update logging

--- c_lang/logging/logging.h
+++ c_lang/logging/logging.h
@@ -10,7 +10,7 @@
 
 #define OUTPUT_FILE stderr
 
-#define CURRENT_LOG_LEVEL LOG_LEVEL_DEBUG
+#define CURRENT_LOG_LEVEL LOG_LEVEL_INFO
 
 // #define ESC_START     "\033["
 // #define ESC_END       "\033[0m"
@@ -29,32 +29,32 @@
     // #define LOG_DEBUG(format, args...) (printf( ESC_START COLOR_INFO "[INFO]-[%s]-[%s]-[%d]:" format ESC_END, __FILE__, __FUNCTION__ , __LINE__, ##args))
     #define LOG_DEBUG(format, args...) (fprintf(OUTPUT_FILE, "DEBUG %s %d %s] " format , __FILE__, __LINE__, __FUNCTION__ , ##args))
 #else
-    #define LOG_DEBUG(format, args...) do{}while(0);
+    #define LOG_DEBUG(format, args...) do{}while(0)
 #endif 
     
 
 #if (LOG_LEVEL_INFO >= CURRENT_LOG_LEVEL)
     #define LOG_INFO(format, args...) (fprintf(OUTPUT_FILE, "INFO  %s %d %s] " format , __FILE__, __LINE__, __FUNCTION__ , ##args))
 #else
-    #define LOG_INFO(format, args...) do{}while(0);
+    #define LOG_INFO(format, args...) do{}while(0)
 #endif
 
 #if (LOG_LEVEL_WARN >= CURRENT_LOG_LEVEL)
     #define LOG_WARN(format, args...) (fprintf(OUTPUT_FILE, "WARN  %s %d %s] " format , __FILE__, __LINE__, __FUNCTION__ , ##args))
 #else
-    #define LOG_WARN(format, args...) do{}while(0);
+    #define LOG_WARN(format, args...) do{}while(0)
 #endif
 
 #if (LOG_LEVEL_ERROR >= CURRENT_LOG_LEVEL)
     #define LOG_ERROR(format, args...) (fprintf(OUTPUT_FILE, "ERROR %s %d %s] " format , __FILE__, __LINE__, __FUNCTION__ , ##args))
 #else
-    #define LOG_ERROR(format, args...) do{}while(0);
+    #define LOG_ERROR(format, args...) do{}while(0)
 #endif
 
 #if (LOG_LEVEL_FATAL >= CURRENT_LOG_LEVEL)
     #define LOG_FATAL(format, args...) (fprintf(OUTPUT_FILE, "FATAL %s %d %s] " format , __FILE__, __LINE__, __FUNCTION__ , ##args))
 #else
-    #define LOG_FATAL(format, args...) do{}while(0);
+    #define LOG_FATAL(format, args...) do{}while(0)
 #endif
 
 
