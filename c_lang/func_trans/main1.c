5d830c80c3f4fb52119ae944ba24be5f8b14c5f2
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2020-11-27 18:17:01 +0800

    main1 looks work

--- c_lang/func_trans/main1.c
+++ c_lang/func_trans/main1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
+#include <stdbool.h>
 
 
 
@@ -13,49 +13,156 @@ typedef long (*func_t)(long arg0, long arg1, long arg2,
         long arg3, long arg4, long arg5);
 
 
-long func1(long arg0)
-{
-    printf("func1: %ld\n", arg0);
-    return 1;
-}
 
-long func2(int arg0, int arg1)
-{
-    printf("func2: %ld, %ld\n", arg0, arg1);
-    return 2;
-}
 
-long func6(long arg0, int arg1, int arg2,
-        int arg3, int arg4, long arg5)
-{
-    printf("func6: %ld, %ld, %ld, %ld, %ld, %ld\n",
-        arg0, arg1, arg2, arg3, arg4, arg5);
-    return 6;
-}
 
-func_t func_array[4] = {
-    (func_t)func1,
-    (func_t)func2,
-    (func_t)func6,
-    NULL
+struct hook_pt_regs {
+    long args[6];
+    long ret;
+    long blank;
 };
 
 
-long hook_func(long arg0, long arg1, long arg2,
+typedef long (*post_func_t)(struct hook_pt_regs* ctx);
+
+// long hook_func(long arg0, long arg1, long arg2,
+//         long arg3, long arg4, long arg5)
+// {
+//     for (int i = 0; i < 4; i++)
+//         if  (func_array[i])
+//         {
+//             long ret = func_array[i](arg0, arg1, arg2, arg3, arg4, arg5);
+//             printf("hook_func: func_ret: %ld\n", ret);
+//         }
+// }
+
+
+typedef void* (*hello_func_t)(long arg0, long arg1, long arg2);
+
+hello_func_t hello_addr;
+
+long hello_func(long arg0, long arg1, long arg2)
+{
+    return (arg0 + arg1 + arg2);
+}
+
+
+long hello_func1(long arg0, long arg1, long arg2)
+{
+    return (arg0 + arg1 + arg2) + 1;
+}
+
+
+
+#define HOOK_ENABLE_POST_RUN 0x1
+
+struct hook_func_t {
+    // func_t func;
+    unsigned long func;
+    unsigned long flag;
+};
+
+struct hook_t {
+    struct hook_func_t prev_func[8];
+    struct hook_func_t post_func[8];
+};
+
+
+struct hook_t hello_hook;
+bool hello_hook_inited = false;
+
+
+long hello_hook_func(long arg0, long arg1, long arg2,
         long arg3, long arg4, long arg5)
 {
-    for (int i = 0; i < 4; i++)
-        if  (func_array[i])
+    long ret;
+
+    for (int i = 0; i < 8; i++)
+        if  (hello_hook.prev_func[i].func)
         {
-            long ret = func_array[i](arg0, arg1, arg2, arg3, arg4, arg5);
-            printf("hook_func: func_ret: %ld\n", ret);
+            ret = ((func_t)(hello_hook.prev_func[i].func))(arg0, arg1, arg2,
+                arg3, arg4, arg5);
+
+            if  (!(hello_hook.prev_func[i].flag & HOOK_ENABLE_POST_RUN))
+                break;
         }
+
+    struct hook_pt_regs ctx = {
+        .args = {arg0, arg1, arg2, arg3, arg4, arg5},
+        .ret = ret,
+        .blank = 0
+    };
+
+    for (int i = 0; i < 8; i++)
+        if  (hello_hook.post_func[i].func)
+        {
+            ((post_func_t)(hello_hook.post_func[i].func))(&ctx);
+            
+            if  (!(hello_hook.post_func[i].flag & HOOK_ENABLE_POST_RUN))
+                break;
+        }
+
+    return ret;
 }
+
+
+
+
+
+// now only for hello_hook
+int hello_register_hook(unsigned long func_addr, unsigned long flag, int index)
+{
+    if  (!hello_hook_inited)
+    {
+        struct hook_func_t origin_func_hook = {
+            .func = func_addr,
+            .flag = flag
+        };
+        hello_hook.prev_func[7] = origin_func_hook;
+        hello_addr = hello_hook_func;
+ 
+        hello_hook_inited = true;
+    }
+
+    if  (index == 7)  return -1;
+
+
+    struct hook_func_t func_hook = {
+        .func = func_addr,
+        .flag = flag
+    };
+
+    if  (index < 8)
+    {   // prev_func
+
+        hello_hook.prev_func[index] = func_hook;
+    }
+    else
+    {
+        index -= 8;
+
+        hello_hook.post_func[index] = func_hook;
+    }
+
+    return 0;
+}
+
 
 
 
 int main()
 {
-    hook_func(1, 2, 3, 4, 5, 6);
-    return 0;
+    // hook_func(1, 2, 3, 4, 5, 6);
+
+    hello_addr = hello_func;
+    long ret = hello_addr(1, 2, 3);
+    printf("ret: %ld\n", ret);
+
+    hello_register_hook((unsigned long)hello_func1, 0, 0);
+    long ret1 = hello_addr(1, 2, 3);
+    printf("ret1: %ld\n", ret1);
+
+
+
+   return 0;
 }
