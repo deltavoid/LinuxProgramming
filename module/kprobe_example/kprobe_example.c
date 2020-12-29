9a29f5522e6ab7f63a75546dc938c34c2d81635d
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2020-12-29 16:32:04 +0800

    change post_handler to NULL

--- module/kprobe_example/kprobe_example.c
+++ module/kprobe_example/kprobe_example.c
@@ -48,61 +48,61 @@ module_param_string(symbol, symbol, sizeof(symbol), 0644);
 
 /* For each probe you need to allocate a kprobe structure */
 static struct kprobe kp = {
-	.symbol_name	= symbol,
+    .symbol_name	= symbol,
 };
 
 /* kprobe pre_handler: called just before the probed instruction is executed */
 static int handler_pre(struct kprobe *p, struct pt_regs *regs)
 {
 #ifdef CONFIG_X86
-	pr_info("<%s> pre_handler: p->addr = 0x%p, ip = %lx, flags = 0x%lx\n",
-		p->symbol_name, p->addr, regs->ip, regs->flags);
+    pr_info("<%s> pre_handler: p->addr = 0x%p, ip = %lx, flags = 0x%lx\n",
+        p->symbol_name, p->addr, regs->ip, regs->flags);
 #endif
 #ifdef CONFIG_PPC
-	pr_info("<%s> pre_handler: p->addr = 0x%p, nip = 0x%lx, msr = 0x%lx\n",
-		p->symbol_name, p->addr, regs->nip, regs->msr);
+    pr_info("<%s> pre_handler: p->addr = 0x%p, nip = 0x%lx, msr = 0x%lx\n",
+        p->symbol_name, p->addr, regs->nip, regs->msr);
 #endif
 #ifdef CONFIG_MIPS
-	pr_info("<%s> pre_handler: p->addr = 0x%p, epc = 0x%lx, status = 0x%lx\n",
-		p->symbol_name, p->addr, regs->cp0_epc, regs->cp0_status);
+    pr_info("<%s> pre_handler: p->addr = 0x%p, epc = 0x%lx, status = 0x%lx\n",
+        p->symbol_name, p->addr, regs->cp0_epc, regs->cp0_status);
 #endif
 #ifdef CONFIG_ARM64
-	pr_info("<%s> pre_handler: p->addr = 0x%p, pc = 0x%lx,"
-			" pstate = 0x%lx\n",
-		p->symbol_name, p->addr, (long)regs->pc, (long)regs->pstate);
+    pr_info("<%s> pre_handler: p->addr = 0x%p, pc = 0x%lx,"
+            " pstate = 0x%lx\n",
+        p->symbol_name, p->addr, (long)regs->pc, (long)regs->pstate);
 #endif
 #ifdef CONFIG_S390
-	pr_info("<%s> pre_handler: p->addr, 0x%p, ip = 0x%lx, flags = 0x%lx\n",
-		p->symbol_name, p->addr, regs->psw.addr, regs->flags);
+    pr_info("<%s> pre_handler: p->addr, 0x%p, ip = 0x%lx, flags = 0x%lx\n",
+        p->symbol_name, p->addr, regs->psw.addr, regs->flags);
 #endif
 
-	/* A dump_stack() here will give a stack backtrace */
-	return 0;
+    /* A dump_stack() here will give a stack backtrace */
+    return 0;
 }
 
 /* kprobe post_handler: called after the probed instruction is executed */
 static void handler_post(struct kprobe *p, struct pt_regs *regs,
-				unsigned long flags)
+                unsigned long flags)
 {
 #ifdef CONFIG_X86
-	pr_info("<%s> post_handler: p->addr = 0x%p, flags = 0x%lx\n",
-		p->symbol_name, p->addr, regs->flags);
+    pr_info("<%s> post_handler: p->addr = 0x%p, flags = 0x%lx\n",
+        p->symbol_name, p->addr, regs->flags);
 #endif
 #ifdef CONFIG_PPC
-	pr_info("<%s> post_handler: p->addr = 0x%p, msr = 0x%lx\n",
-		p->symbol_name, p->addr, regs->msr);
+    pr_info("<%s> post_handler: p->addr = 0x%p, msr = 0x%lx\n",
+        p->symbol_name, p->addr, regs->msr);
 #endif
 #ifdef CONFIG_MIPS
-	pr_info("<%s> post_handler: p->addr = 0x%p, status = 0x%lx\n",
-		p->symbol_name, p->addr, regs->cp0_status);
+    pr_info("<%s> post_handler: p->addr = 0x%p, status = 0x%lx\n",
+        p->symbol_name, p->addr, regs->cp0_status);
 #endif
 #ifdef CONFIG_ARM64
-	pr_info("<%s> post_handler: p->addr = 0x%p, pstate = 0x%lx\n",
-		p->symbol_name, p->addr, (long)regs->pstate);
+    pr_info("<%s> post_handler: p->addr = 0x%p, pstate = 0x%lx\n",
+        p->symbol_name, p->addr, (long)regs->pstate);
 #endif
 #ifdef CONFIG_S390
-	pr_info("<%s> pre_handler: p->addr, 0x%p, flags = 0x%lx\n",
-		p->symbol_name, p->addr, regs->flags);
+    pr_info("<%s> pre_handler: p->addr, 0x%p, flags = 0x%lx\n",
+        p->symbol_name, p->addr, regs->flags);
 #endif
 }
 
@@ -113,31 +113,32 @@ static void handler_post(struct kprobe *p, struct pt_regs *regs,
  */
 static int handler_fault(struct kprobe *p, struct pt_regs *regs, int trapnr)
 {
-	pr_info("fault_handler: p->addr = 0x%p, trap #%dn", p->addr, trapnr);
-	/* Return 0 because we don't handle the fault. */
-	return 0;
+    pr_info("fault_handler: p->addr = 0x%p, trap #%dn", p->addr, trapnr);
+    /* Return 0 because we don't handle the fault. */
+    return 0;
 }
 
 static int __init kprobe_init(void)
 {
-	int ret;
-	kp.pre_handler = handler_pre;
-	kp.post_handler = handler_post;
-	kp.fault_handler = handler_fault;
+    int ret;
+    kp.pre_handler = handler_pre;
+    // kp.post_handler = handler_post;
+    kp.post_handler = NULL;
+    kp.fault_handler = handler_fault;
 
-	ret = register_kprobe(&kp);
-	if (ret < 0) {
-		pr_err("register_kprobe failed, returned %d\n", ret);
-		return ret;
-	}
-	pr_info("Planted kprobe at %p\n", kp.addr);
-	return 0;
+    ret = register_kprobe(&kp);
+    if (ret < 0) {
+        pr_err("register_kprobe failed, returned %d\n", ret);
+        return ret;
+    }
+    pr_info("Planted kprobe at %p\n", kp.addr);
+    return 0;
 }
 
 static void __exit kprobe_exit(void)
 {
-	unregister_kprobe(&kp);
-	pr_info("kprobe at %p unregistered\n", kp.addr);
+    unregister_kprobe(&kp);
+    pr_info("kprobe at %p unregistered\n", kp.addr);
 }
 
 module_init(kprobe_init)
