b0a0118a1fe521d51a5cb51b926d9ba91787bab9
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2020-12-29 16:35:38 +0800

    change hook to __do_softirq

--- module/kprobe_example/kprobe_example.c
+++ module/kprobe_example/kprobe_example.c
@@ -54,57 +54,61 @@ static struct kprobe kp = {
 /* kprobe pre_handler: called just before the probed instruction is executed */
 static int handler_pre(struct kprobe *p, struct pt_regs *regs)
 {
-#ifdef CONFIG_X86
-    pr_info("<%s> pre_handler: p->addr = 0x%p, ip = %lx, flags = 0x%lx\n",
-        p->symbol_name, p->addr, regs->ip, regs->flags);
-#endif
-#ifdef CONFIG_PPC
-    pr_info("<%s> pre_handler: p->addr = 0x%p, nip = 0x%lx, msr = 0x%lx\n",
-        p->symbol_name, p->addr, regs->nip, regs->msr);
-#endif
-#ifdef CONFIG_MIPS
-    pr_info("<%s> pre_handler: p->addr = 0x%p, epc = 0x%lx, status = 0x%lx\n",
-        p->symbol_name, p->addr, regs->cp0_epc, regs->cp0_status);
-#endif
-#ifdef CONFIG_ARM64
-    pr_info("<%s> pre_handler: p->addr = 0x%p, pc = 0x%lx,"
-            " pstate = 0x%lx\n",
-        p->symbol_name, p->addr, (long)regs->pc, (long)regs->pstate);
-#endif
-#ifdef CONFIG_S390
-    pr_info("<%s> pre_handler: p->addr, 0x%p, ip = 0x%lx, flags = 0x%lx\n",
-        p->symbol_name, p->addr, regs->psw.addr, regs->flags);
-#endif
+// #ifdef CONFIG_X86
+//     pr_info("<%s> pre_handler: p->addr = 0x%p, ip = %lx, flags = 0x%lx\n",
+//         p->symbol_name, p->addr, regs->ip, regs->flags);
+// #endif
+// #ifdef CONFIG_PPC
+//     pr_info("<%s> pre_handler: p->addr = 0x%p, nip = 0x%lx, msr = 0x%lx\n",
+//         p->symbol_name, p->addr, regs->nip, regs->msr);
+// #endif
+// #ifdef CONFIG_MIPS
+//     pr_info("<%s> pre_handler: p->addr = 0x%p, epc = 0x%lx, status = 0x%lx\n",
+//         p->symbol_name, p->addr, regs->cp0_epc, regs->cp0_status);
+// #endif
+// #ifdef CONFIG_ARM64
+//     pr_info("<%s> pre_handler: p->addr = 0x%p, pc = 0x%lx,"
+//             " pstate = 0x%lx\n",
+//         p->symbol_name, p->addr, (long)regs->pc, (long)regs->pstate);
+// #endif
+// #ifdef CONFIG_S390
+//     pr_info("<%s> pre_handler: p->addr, 0x%p, ip = 0x%lx, flags = 0x%lx\n",
+//         p->symbol_name, p->addr, regs->psw.addr, regs->flags);
+// #endif
+
+    unsigned preempt_cnt = preempt_count();
+    if  (preempt_cnt)
+        pr_debug("preempt_count: %x\n", preempt_cnt);
 
     /* A dump_stack() here will give a stack backtrace */
     return 0;
 }
 
-/* kprobe post_handler: called after the probed instruction is executed */
-static void handler_post(struct kprobe *p, struct pt_regs *regs,
-                unsigned long flags)
-{
-#ifdef CONFIG_X86
-    pr_info("<%s> post_handler: p->addr = 0x%p, flags = 0x%lx\n",
-        p->symbol_name, p->addr, regs->flags);
-#endif
-#ifdef CONFIG_PPC
-    pr_info("<%s> post_handler: p->addr = 0x%p, msr = 0x%lx\n",
-        p->symbol_name, p->addr, regs->msr);
-#endif
-#ifdef CONFIG_MIPS
-    pr_info("<%s> post_handler: p->addr = 0x%p, status = 0x%lx\n",
-        p->symbol_name, p->addr, regs->cp0_status);
-#endif
-#ifdef CONFIG_ARM64
-    pr_info("<%s> post_handler: p->addr = 0x%p, pstate = 0x%lx\n",
-        p->symbol_name, p->addr, (long)regs->pstate);
-#endif
-#ifdef CONFIG_S390
-    pr_info("<%s> pre_handler: p->addr, 0x%p, flags = 0x%lx\n",
-        p->symbol_name, p->addr, regs->flags);
-#endif
-}
+// /* kprobe post_handler: called after the probed instruction is executed */
+// static void handler_post(struct kprobe *p, struct pt_regs *regs,
+//                 unsigned long flags)
+// {
+// #ifdef CONFIG_X86
+//     pr_info("<%s> post_handler: p->addr = 0x%p, flags = 0x%lx\n",
+//         p->symbol_name, p->addr, regs->flags);
+// #endif
+// #ifdef CONFIG_PPC
+//     pr_info("<%s> post_handler: p->addr = 0x%p, msr = 0x%lx\n",
+//         p->symbol_name, p->addr, regs->msr);
+// #endif
+// #ifdef CONFIG_MIPS
+//     pr_info("<%s> post_handler: p->addr = 0x%p, status = 0x%lx\n",
+//         p->symbol_name, p->addr, regs->cp0_status);
+// #endif
+// #ifdef CONFIG_ARM64
+//     pr_info("<%s> post_handler: p->addr = 0x%p, pstate = 0x%lx\n",
+//         p->symbol_name, p->addr, (long)regs->pstate);
+// #endif
+// #ifdef CONFIG_S390
+//     pr_info("<%s> pre_handler: p->addr, 0x%p, flags = 0x%lx\n",
+//         p->symbol_name, p->addr, regs->flags);
+// #endif
+// }
 
 /*
  * fault_handler: this is called if an exception is generated for any
