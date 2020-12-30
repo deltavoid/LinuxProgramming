577e9d5d06d4bf1f9a55c56ea56c37b50887c328
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2020-12-30 11:53:11 +0800

    add mutex usage

--- module/mutex_use/mutex_use.c
+++ module/mutex_use/mutex_use.c
@@ -8,15 +8,29 @@
 #include <linux/version.h>
 
 
+
+struct mutex mutex;
+
+
 static int __init mutex_use_init(void)
 {
     pr_info("mutex_use inserted\n");
+
+    mutex_init(&mutex);
+
+    mutex_lock(&mutex);
+
+    mutex_unlock(&mutex);
+
     return 0;
 }
 
 static void __exit mutex_use_exit(void)
 {
     pr_info("mutex_use removed\n");
+
+    mutex_destroy(&mutex);
+
     pr_debug("-------------------------------------------------\n");
 }
 
