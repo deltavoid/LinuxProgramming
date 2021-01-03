d18b3da3c20ae4c75b45cc79651b23ff3e8f931a
root <root@bytedance>
2021-01-03 16:31:26 +0800

    update hello

--- module/hello/hello.c
+++ module/hello/hello.c
@@ -8,15 +8,25 @@
 #include <linux/version.h>
 
 
+
+
+// module init ----------------------------------------------------------
+
 static int __init hello_init(void)
 {
-    pr_info("hello inserted\n");
+    pr_info("hello_init begin\n");
+
+
+    pr_info("hello_init end\n");
     return 0;
 }
 
 static void __exit hello_exit(void)
 {
-    pr_info("hello removed\n");
+    pr_info("hello_exit begin\n");
+
+
+    pr_info("hello_exit end\n");
     pr_debug("-------------------------------------------------\n");
 }
 
