06d194b5a0a2b8743480ba95b89a6617b7a0de3b
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-01-03 16:33:28 +0800

    add tracepoint_example

---
+++ module/tracepoint_example/tracepoint_example.c
@@ -0,0 +1,147 @@
+
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/version.h>
+
+#include <linux/tracepoint.h>
+#include <trace/events/sched.h>
+
+// probe sched ----------------------------------------------------
+
+
+static void probe_sched_wakeup(void *priv, struct task_struct *p)
+{
+    pr_debug("probe_sched_wakeup: wakeup %s\n", p->comm);
+}
+
+// tracepoint_probe_context ----------------------------------------
+
+
+struct tracepoint_probe_entry {
+    char* name;
+    struct tracepoint *tp;
+    void* probe;
+    void* priv;
+};
+
+#define TRACEPOINT_PROBE_CONTEXT_MAX_NUM 8
+
+struct tracepoint_probe_context {
+    struct tracepoint_probe_entry entries[TRACEPOINT_PROBE_CONTEXT_MAX_NUM];
+    int init_num;
+    int found_num;
+};
+
+static void tracepoint_lookup(struct tracepoint *tp, void *priv)
+{
+    int i;
+    struct tracepoint_probe_context* ctx = priv;
+
+    if  (ctx->found_num == ctx->init_num)
+        return;
+
+    for (i = 0; i < ctx->init_num; i++)
+    {   struct tracepoint_probe_entry* entry = &ctx->entries[i];
+
+        if  (entry->tp || strcmp(tp->name, entry->name))
+            continue;
+        entry->tp = tp;
+        ctx->found_num++;
+    }
+}
+
+static int tracepoint_probe_context_find_tracepoints(struct tracepoint_probe_context* ctx)
+{
+    int i;
+    for (i = 0; i < ctx->init_num; i++)
+        ctx->entries[i].tp = NULL;
+    ctx->found_num = 0;
+
+
+    for_each_kernel_tracepoint(tracepoint_lookup, ctx);
+
+    if  (ctx->found_num != ctx->init_num)
+        return -1;
+    return 0;
+}
+
+static int tracepoint_probe_context_register_probes(struct tracepoint_probe_context* ctx)
+{
+    int i;
+
+    for (i = 0; i < ctx->init_num; i++)
+    {   struct tracepoint_probe_entry* entry = &ctx->entries[i];
+        int ret = tracepoint_probe_register(entry->tp, entry->probe, entry->priv);
+        if  (ret)
+            pr_warn("trace_%s probe failed\n", entry->name);
+
+    }
+
+    return 0;
+}
+
+static void tracepoint_probe_context_unregister_probes(struct tracepoint_probe_context* ctx)
+{
+    int i;
+
+    for (i = 0; i < ctx->init_num; i++)
+    {   struct tracepoint_probe_entry* entry = &ctx->entries[i];
+        tracepoint_probe_unregister(entry->tp, entry->probe, entry->priv);
+    }
+
+    tracepoint_synchronize_unregister();
+}
+
+
+// module init -----------------------------------------------------
+
+static struct tracepoint_probe_context sched_probes = {
+    .entries = {
+        {
+            .name = "sched_wakeup",
+            .probe = probe_sched_wakeup,
+            .priv = NULL,
+        }
+    },
+    .init_num = 1
+};
+
+
+
+static int __init tracepoint_init(void)
+{
+    pr_debug("tracepoint_init begin\n");
+
+    if  (tracepoint_probe_context_find_tracepoints(&sched_probes) < 0)
+    {   pr_warn("find tracepoints failed\n");
+        return -1;
+    }
+
+    if  (tracepoint_probe_context_register_probes(&sched_probes) < 0)
+    {   pr_warn("register probes failed\n");
+        return -1;
+    }
+
+
+    pr_debug("tracepoint_init end\n");
+    return 0;
+}
+
+static void __exit tracepoint_exit(void)
+{
+    pr_debug("tracepoint_exit begin\n");
+
+    tracepoint_probe_context_unregister_probes(&sched_probes);
+
+
+    pr_debug("tracepoint_exit end\n");
+    pr_debug("------------------------------------------------------------------\n");
+}
+
+module_init(tracepoint_init)
+module_exit(tracepoint_exit)
+MODULE_LICENSE("GPL");
