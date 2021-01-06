4c1bbe2df188921836df62d2065d010be8d5d914
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-01-06 19:32:06 +0800

    add test_irq

--- module/irq_request_example/irq_request_example.c
+++ module/irq_request_example/irq_request_example.c
@@ -34,6 +34,24 @@ static uint64_t count;
 int irq_line;
 
 
+static void test_irq(void)
+{
+    int i;
+
+    for (i = 0; i < 100; i++)
+    {
+        if  (request_irq(i, example_interrupt, IRQF_SHARED, "test", &count) < 0)
+        {
+            pr_debug("request_irq %d failed\n", i);
+            continue;
+        }
+
+        pr_debug("request_irq %d success\n", i);
+        free_irq(i, &count);
+    }
+}
+
+
 static int __init irq_request_init(void)
 {
     int i;
@@ -44,18 +62,20 @@ static int __init irq_request_init(void)
     // {   pr_warn("request_irq failed\n");
     //     return -1;
     // }
-    irq_line = -1;
-    for (i = 0; i < 100; i++)
-    {
-        if  (request_irq(i, example_interrupt, IRQF_SHARED, "test", &count) == 0)
-        {   pr_debug("request_irq %d success\n", i);
-            irq_line = i;
-            break;
-        }
-    }
+    // irq_line = -1;
+    // for (i = 0; i < 100; i++)
+    // {
+    //     if  (request_irq(i, example_interrupt, IRQF_SHARED, "test", &count) == 0)
+    //     {   pr_debug("request_irq %d success\n", i);
+    //         irq_line = i;
+    //         break;
+    //     }
+    // }
 
-    if  (irq_line < 0)
-        return -1;
+    // if  (irq_line < 0)
+    //     return -1;
+
+    test_irq();
 
     pr_info("irq_request_init end\n");
     return 0;
@@ -65,7 +85,7 @@ static void __exit irq_request_exit(void)
 {
     pr_info("irq_request_exit begin\n");
 
-    free_irq(irq_line, &count);
+    // free_irq(irq_line, &count);
 
 
     pr_info("irq_request_exit end\n");
