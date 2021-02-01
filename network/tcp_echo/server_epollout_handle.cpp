4034199a32f7ce57fd5083b68b08b68ea4c414f5
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-02-01 18:22:40 +0800

    add server_epollout_handle with parse_args

--- network/tcp_echo/server_epollout_handle.cpp
+++ network/tcp_echo/server_epollout_handle.cpp
@@ -3,18 +3,70 @@
 #include <cstdint>
 #include <cstring>
 
+#include <thread>
+#include <memory>
+#include <vector>
+#include <map>
+
 
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/epoll.h>
 #include <sys/socket.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 
 
+const int max_conn = 1024;
+const int max_events = max_conn;
+const int max_pkt_size = 1024;
 
 
-int main()
+struct sockaddr_in local_addr;
+int thread_num = 1;
+int conn_num = 1;
+int pkt_size = 64;
+int duration = 10;
+
+
+int parse_args(int argc, char** argv)
+{
+    if  (argc < 2)
+    {   printf("usage: %s <local port> [<threads>]\n", argv[0]);
+        return -1;
+    }
+
+    int port_ = 0;
+    if  (sscanf(argv[1], "%d", &port_) < 0)
+    {   perror("bad port");
+        return -1;
+    }
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_addr.s_addr = INADDR_ANY;
+    local_addr.sin_port = htons((short)port_);
+    bzero(&local_addr.sin_zero, sizeof(local_addr.sin_zero));
+
+    if  (argc > 2)
+    {   if  (sscanf(argv[2], "%d", &thread_num) < 0)
+        {   perror("bad thread_num");
+            return -1;
+        }
+    }
+
+    printf("local_port: %d, thread_num: %d\n", port_, thread_num);
+    return 0;
+}
+
+
+int main(int argc, char** argv)
 {
     printf("hello world\n");
+    if  (parse_args(argc, argv) < 0)
+        return 1;
+
+
     return 0;
 }
